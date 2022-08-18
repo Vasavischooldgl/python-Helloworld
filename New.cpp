cfe779bcb70953903ebe641430c115580f78ec32
VASAVI MATRICULATION HR SEC SCHOOL, Dindigul <info@vasavimatric.com>
2022-08-18 17:35:48 +0530

    Create New.cpp

---
+++ New.cpp
@@ -0,0 +1,5 @@
+android.graphics.drawable.GradientDrawable CTVSUB= new android.graphics.drawable.GradientDrawable();
+CTVSUB.setCornerRadius(40);
+CTVSUB.setColor(Color.parseColor("#FF000000")); 
+imageview1.setBackground(CTVSUB);
+imageview1.setClipToOutline(true);
