771651f4c96d8c671d952dffb10b694ea1b23e4b
VASAVI MATRICULATION HR SEC SCHOOL, Dindigul <info@vasavimatric.com>
2022-08-18 17:33:10 +0530

    Update New.py

--- New.py
+++ New.py
@@ -1,1 +1,27 @@
-print("Hello-World")
+class BA():
+ class  Gokul():
+  class Utils():
+    def Write(sk):
+     print(sk)
+    def Rule(dk):
+   	 print(dk)   
+    class strings():
+          def details(word,vars):
+           index = vars.find(word)
+           lens = len(word)
+           print(index)
+           print(vars[index:index+lens])
+           a = index
+           b = index+lens
+           print("stringbetween",a,":",b)     
+s = "Why Has Been Held That"   
+gstring = BA.Gokul.Utils.strings.details
+gstring("That",s)
+gstring("Why",s)
+gstring("Been",s)
+gs = BA.Gokul.Utils.strings.details
+gs("Why",s)
+gs("Been",s)
+Dp = BA.Gokul.Utils
+Dp.Write("Hello-World")
+Dp.Write("strin {},{},{}".format(100,200,gs("Why",s)))
